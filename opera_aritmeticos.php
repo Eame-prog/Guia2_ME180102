3b11b1bbf78a408ffd855223047b2c0346ff3f75
Eame-prog <88166948+Eame-prog@users.noreply.github.com>
2022-02-05 10:58:36 -0600

    Add files via upload

---
+++ opera_aritmeticos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Ejemplo de PHP - Operadores aritmeticos</title>
+	<style>
+		body{
+			font-family: "Arial Black";
+		}
+	</style>
+</head>
+<body>
+<?php
+	$a = 8;
+	$b = 3;
+	echo $a + $b, "<br>";
+	echo $a - $b, "<br>";
+	echo $a * $b, "<br>";
+	echo $a / $b, "<br>";
+	$a++;
+	echo $a,"<br>";
+	$b--;
+	echo $b, "<br>";
+	?>
+</body>
+</html>
---
+++ uso_variable.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Ejemplo de PHP</title>
+	<style>
+		body{
+			font-family: "Arial Black";
+		}
+	</style>
+</head>
+<body>
+	<?php
+		$a = 1;
+		$b = 3.34;
+		$c = "Hola Mundo";
+		echo $a, "<br>", $b, "<br>", $c;
+	?>
+</body>
+</html>
